2d5168a0d01fe1994ba030018f357946863c6f31
Matheus Ferreira <matheus.ferreirraa@gmail.com>
2015-09-20 02:22:14 -0300

    master: melhoria e correcoes nos testes de movimento

--- main.c
+++ main.c
@@ -16,6 +16,11 @@ struct disco {
     int anilha_next;
 };
 
+void print(struct disco v1, struct disco v2) {
+    printf("movendo de %d para %d\n com dados:\n", v1.anilha_current, v2.anilha_current);
+    printf("valor: %d\n atual: %d\n ultima %d\n proxima: %d\n", v1.value, v1.anilha_current, v1.anilha_last, v1.anilha_next);
+}
+
 void inicializa(struct disco *tower, struct disco *tower_aux, struct disco *tower_final, int n_real) {
 
     int i;
@@ -42,6 +47,7 @@ void inicializa(struct disco *tower, struct disco *tower_aux, struct disco *towe
 int lastDiscTower(struct disco *tower, int n_real) {
     int i;
     for (i = 0; i <= n_real; i++) {
+        printf("%d: %d\n", i, tower[i].value);
         if (tower[i].value == -1) {
             return i - 1;
         }
@@ -55,12 +61,11 @@ int verificaMovimento(struct disco *v1, struct disco *v2, int n_real) {
     int pos2 = lastDiscTower(v2, n_real);
 
     if (v2[pos2].value == -1 || v1[pos1].value < v2[pos2].value) {
-        return 1;
+        if (v1[pos1].anilha_next == v2[pos2].anilha_current || v1[pos1].anilha_next == -1) {
+            return 1;
+        }
     }
-}
-
-int doMove(int last_move, struct disco *v1, struct disco *v2) {
-
+    return 0;
 }
 
 void hanoi(int n) {
@@ -70,9 +75,13 @@ void hanoi(int n) {
             tower_aux[n],
             tower_final[n];
 
-    struct disco discoNull = {-1, -1, -1, -1, -1};
+    struct disco discoNull = {-1, -1, -1, -1};
 
-    int i, n_real = n - 1, last_move = 0, pos;
+    int i, n_real = n - 1,
+            last_move = 0,
+            last1,
+            last2,
+            last3;
 
     inicializa(tower, tower_aux, tower_final, n_real);
 
@@ -91,21 +100,76 @@ void hanoi(int n) {
     }
     tower[n_real].value = -1;
 
-    while (tower_final[n_real].value != -1) {
+    while (tower_final[n_real].value == -1) {
+        last1 = lastDiscTower(tower, n_real);
+        last2 = lastDiscTower(tower_aux, n_real);
+        last3 = lastDiscTower(tower_final, n_real);
+        printf("%d\n%d\n%d\n", last1, last2, last3);
         if (last_move == 2) {
-            pos = lastDiscTower(tower, n_real);
-            // realizar mudança de acordo com o proximo que a estrutura do disco indica
-            if (tower[pos].anilha_next ==) {
-
-            }
             if (verificaMovimento(tower, tower_final, n_real)) {
-                if (tower[pos].anilha_last)
-                    doMove(last_move,);
+                tower[last1].anilha_current = 3;
+                tower[last1].anilha_last = 1;
+                tower[last1].anilha_next = 2;
+                //                print(tower[last1], tower_final[last3 + 1]);
+                tower_final[last3 + 1] = tower[last1];
+                discoNull.anilha_current = 1;
+                tower[last1] = discoNull;
+                last_move = 3;
+            } else if (verificaMovimento(tower_final, tower, n_real)) {
+                tower_final[last3].anilha_current = 1;
+                tower_final[last3].anilha_last = 3;
+                tower_final[last3].anilha_next = 2;
+
+                tower[last1 + 1] = tower_final[last3];
+                discoNull.anilha_current = 3;
+                tower_final[last3] = discoNull;
+                last_move = 1;
+            } else if (verificaMovimento(tower, tower_aux, n_real)) {
+                tower[last1].anilha_current = 2;
+                tower[last1].anilha_last = 1;
+                tower[last1].anilha_next = 3;
+
+                tower_aux[last2 + 1] = tower[last1];
+                discoNull.anilha_current = 1;
+                tower[last1] = discoNull;
+                last_move = 2;
             }
         } else if (last_move == 3) {
+            if (verificaMovimento(tower, tower_aux, n_real)) {
+                tower[last1].anilha_current = 2;
+                tower[last1].anilha_last = 1;
+                tower[last1].anilha_next = 3;
 
+                tower_aux[last2 + 1] = tower[last1];
+                discoNull.anilha_current = 1;
+                tower[last1] = discoNull;
+                last_move = 2;
+            } else if (verificaMovimento(tower_aux, tower_final, n_real)) {
+                tower_aux[last2].anilha_current = 3;
+                tower_aux[last2].anilha_last = 2;
+                tower_aux[last2].anilha_next = 1;
+
+                tower_final[last3 + 1] = tower_aux[last2];
+                discoNull.anilha_current = 2;
+                tower_aux[last2] = discoNull;
+                last_move = 3;
+            }
+        } else if (last_move == 1) {
+            if (verificaMovimento(tower_final, tower_aux, n_real)) {
+                tower_final[last3].anilha_current = 2;
+                tower_final[last3].anilha_last = 3;
+                tower_final[last3].anilha_next = 1;
+
+                tower_aux[last2 + 1] = tower_final[last3];
+                discoNull.anilha_current = 3;
+                tower_final[last3] = discoNull;
+                last_move = 2;
+            }
         }
-
+    }
+    printf("parece q deu aqui\n\n\n");
+    for (i = 0; i <= n_real; i++) {
+        printf("%d : %d\n", i, tower_final[i].value);
     }
 }
 
